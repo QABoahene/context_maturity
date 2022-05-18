e37cef3420802770a0877cde45da93ea0334f777
Quarshie Adu Boahene <qaboahene@Quarshies-MBP.broadband>
2022-05-18 18:13:15 +0100

    context_maturity

---
+++ python files/data_preparation.py
@@ -0,0 +1,11 @@
+# Needed modules
+import pandas as pd
+
+# Data Preparation function
+## Ask where album folder is
+## List album folders there
+## Select artist
+## Import the data
+## Add album name column
+## combine all album songs
+### That is the data to be cleaned
